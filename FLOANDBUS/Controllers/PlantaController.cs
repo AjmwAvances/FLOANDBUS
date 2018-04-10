9269c510a9a275d6e5f630f477f0274b96bd9ea2
AjmwAvances <38163125+AjmwAvances@users.noreply.github.com>
2018-04-09 22:01:40 -0500

    floandbus
    Para mi clase de informatica avansada

---
+++ FLOANDBUS/Controllers/PlantaController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FLOANDBUS.Controllers
+{
+    public class PlantaController : Controller
+    {
+        //
+        // GET: /Planta/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Crear()
+        {
+            return View();
+        }
+	}
+}
